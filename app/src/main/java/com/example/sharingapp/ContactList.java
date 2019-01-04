dbdd3741336d048f1cd9c255b759ae71c690f9a5
Marcos Leonel <marcos@zeonsolutions.com.br>
2019-01-04 14:31:13 -0200

    [ContactList] class and constructor created

--- app/src/main/java/com/example/sharingapp/ContactList.java
+++ app/src/main/java/com/example/sharingapp/ContactList.java
@@ -10,4 +10,23 @@ public class ContactList {
     public ContactList() {
         contacts = new ArrayList<Contact>();
     }
+
+    public void setContacts(ArrayList<Contact> contacts_list) {
+        this.contacts = contacts_list;
+    }
+
+    public ArrayList<Contact> getContacts() {
+        return contacts;
+    }
+
+    public ArrayList<String> getAllUserNames() {
+
+        ArrayList<String> usernamesList;
+
+        for (String contact: contacts) {
+            String username = contact.getUsername();
+
+            usernamesList.add(contact.username);
+        }
+    }
 }
