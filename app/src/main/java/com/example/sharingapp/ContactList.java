07b00d97bc0b9da6260bebd97ae67550dcec3b37
Marcos Leonel <marcos@zeonsolutions.com.br>
2019-01-04 14:09:27 -0200

    Initial commit

---
+++ app/src/main/java/com/example/sharingapp/ContactList.java
@@ -0,0 +1,13 @@
+package com.example.sharingapp;
+
+import java.util.ArrayList;
+
+public class ContactList {
+
+    private ArrayList<Contact> contacts;
+    private String FILENAME;
+
+    public ContactList() {
+        contacts = new ArrayList<Contact>();
+    }
+}
